61192783ea371c503a386f251684375e6f24f7cd
Shivam Bhandari <168291459+shivtf@users.noreply.github.com>
2026-05-25 18:08:14 +0530

    login screen, signup ,splash screen, theme controller,

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:online_travel_agency/app.dart';
 
 import 'package:online_travel_agency/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WanderlyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
